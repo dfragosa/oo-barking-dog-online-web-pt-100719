70f3469bad7883b7e8d527f7dc366d1c113e63bc
Derric Fragosa <fderric@gmail.com>
2019-10-27 13:25:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,16 @@
 # Your code goes here!
 class Dog 
+  def bark
+    puts "woof!"
+  end 
+  
+  def name=(dogs_name)
+    @dog_name = dogs_name
+  end 
+  
+  def name 
+    @dog_name 
+  end 
   
 end 
+
